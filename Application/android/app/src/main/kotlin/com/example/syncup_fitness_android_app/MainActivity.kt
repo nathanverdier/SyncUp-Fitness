ec74c844b7fcfbd5f88e33bc0cfa301c01d680ef
Nathan <nathanverdier34@gmail.com>
2024-10-27 02:08:22 +0200

    :tada: Init: Mobile App

---
+++ Application/android/app/src/main/kotlin/com/example/syncup_fitness_android_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.syncup_fitness_android_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- Syncup_fitness_app/android/app/src/main/kotlin/com/example/syncup_fitness_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.syncup_fitness_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
