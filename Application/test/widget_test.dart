81aff6e2449a86017e3e1e4b8dc6420e323e98a4
Nathan <nathanverdier34@gmail.com>
2024-12-27 00:20:50 +0100

    🚚 Refactor: Update import path for main.dart to reflect project name change

--- Application/test/widget_test.dart
+++ Application/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:syncup_fitness_android_app/main.dart';
+import 'package:syncup_fitness/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
