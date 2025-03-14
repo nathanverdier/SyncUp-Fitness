ed2352a0d3da17201dc630097c05acb3644d55e4
Nathan <nathanverdier34@gmail.com>
2025-03-14 19:15:24 +0100

    Add: Bluetooth functionality to send commands to Raspberry Pi and update exercise list

--- Application/lib/screens/exercise_page.dart
+++ Application/lib/screens/exercise_page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class ExercisePage extends StatelessWidget {
   const ExercisePage({super.key});
@@ -11,6 +13,9 @@ class ExercisePage extends StatelessWidget {
         MediaQuery.of(context).orientation == Orientation.landscape;
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercises'),
+      ),
       body: Container(
         color: Theme.of(context).colorScheme.primaryContainer, // Fond bleu
         padding: const EdgeInsets.all(16.0),
@@ -25,7 +30,7 @@ class ExercisePage extends StatelessWidget {
               crossAxisSpacing: 20, // Espacement horizontal entre les boutons
               childAspectRatio: 3, // Ratio largeur/hauteur pour les boutons
             ),
-            itemCount: 4, // Nombre total de boutons
+            itemCount: 6, // Nombre total de boutons
             itemBuilder: (context, index) {
               // Liste des exercices et images associées
               final exercises = [
@@ -33,6 +38,11 @@ class ExercisePage extends StatelessWidget {
                 {'title': 'Pull-ups', 'image': 'assets/images/pullup.gif'},
                 {'title': 'Deadlift', 'image': 'assets/images/deadlift.gif'},
                 {'title': 'Bench press', 'image': 'assets/images/bench.gif'},
+                {
+                  'title': 'Biceps Curls',
+                  'image': 'assets/images/biceps_curl.gif'
+                },
+                {'title': 'Treadmill', 'image': 'assets/images/treadmill.gif'},
               ];
 
               return _buildExerciseButton(
@@ -51,7 +61,7 @@ class ExercisePage extends StatelessWidget {
       BuildContext context, String title, String imagePath) {
     return ElevatedButton(
       onPressed: () {
-        // Afficher la popup au clic
+        // Afficher la popup au clic et envoyer la requête Bluetooth
         _showExerciseDialog(context, title);
       },
       style: ElevatedButton.styleFrom(
@@ -92,11 +102,14 @@ class _ExerciseDialog extends StatefulWidget {
 class _ExerciseDialogState extends State<_ExerciseDialog> {
   int secondsElapsed = 0;
   Timer? timer;
+  BluetoothDevice? _device;
+  BluetoothCharacteristic? _characteristic;
 
   @override
   void initState() {
     super.initState();
     _startTimer();
+    _sendBluetoothRequest(widget.title);
   }
 
   // Démarrage du timer
@@ -116,9 +129,57 @@ class _ExerciseDialogState extends State<_ExerciseDialog> {
     }
   }
 
+  // Envoyer une requête Bluetooth à la Raspberry Pi
+  void _sendBluetoothRequest(String command) async {
+    var statusScan = await Permission.bluetoothScan.request();
+    var statusConnect = await Permission.bluetoothConnect.request();
+    var statusLocation = await Permission.locationWhenInUse.request();
+
+    if (statusScan.isGranted &&
+        statusConnect.isGranted &&
+        statusLocation.isGranted) {
+      // Start scanning
+      FlutterBluePlus.startScan(timeout: Duration(seconds: 4));
+
+      // Listen to scan results
+      FlutterBluePlus.scanResults.listen((results) async {
+        for (ScanResult r in results) {
+          if (r.device.name == 'YourRaspberryPiDeviceName') {
+            await FlutterBluePlus.stopScan();
+            _device = r.device;
+            await _device!.connect();
+            List<BluetoothService> services = await _device!.discoverServices();
+            for (var service in services) {
+              for (var characteristic in service.characteristics) {
+                if (characteristic.uuid
+                    .toString()
+                    .contains("00001101-0000-1000-8000-00805F9B34FB")) {
+                  _characteristic = characteristic;
+                  await _characteristic!.write(command.codeUnits);
+                  return;
+                }
+              }
+            }
+          }
+        }
+      });
+    }
+  }
+
+  // Envoyer une requête pour arrêter le programme sur la Raspberry Pi
+  void _sendStopRequest() async {
+    if (_characteristic != null) {
+      await _characteristic!.write("STOP".codeUnits);
+    }
+    if (_device != null) {
+      await _device!.disconnect();
+    }
+  }
+
   @override
   void dispose() {
-    _stopTimer(); // Assurez-vous d'annuler le timer lorsque le widget est supprimé
+    _stopTimer();
+    _sendStopRequest();
     super.dispose();
   }
 
